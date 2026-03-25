ffd291d90dfdf1bb937c1c775d15d9feb62ab6b9
SaiKarthikYamavaram <ysaikarthiksep01@gmail.com>
2026-03-25 22:56:18 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "org.karthik"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+kotlin {
+    jvmToolchain(22)
+}
